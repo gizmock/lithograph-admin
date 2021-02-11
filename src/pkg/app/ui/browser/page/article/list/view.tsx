aa9a7444032f5049c5415dad4173a1aa95ab2aca
kent-hamaguchi <ismism0@gmail.com>
2021-02-11 10:35:50 +0900

    article list

--- src/pkg/app/ui/browser/page/article/list/view.tsx
+++ src/pkg/app/ui/browser/page/article/list/view.tsx
@@ -1,25 +1,57 @@
+import { Button, Card, ControlGroup, H5, InputGroup } from "@blueprintjs/core";
 import { useDidMount } from "beautiful-react-hooks";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { UsecaseContext } from "../../../context";
+import { ArticleEditPath } from "../../../route-path";
 import { ArticleListStateContext } from "./context";
 
 export const ArticleListView = () => {
   const state = useContext(ArticleListStateContext);
   const query = useContext(UsecaseContext).article.query;
+  const title = useRef({ value: "" } as HTMLInputElement);
+
+  const findArticlesByTitle = async (title: string) => {
+    const result = await query.findByTitle(title);
+    state.setArticles(result.datas);
+  };
 
   useDidMount(async () => {
-    const result = await query.findByTitle("");
-    state.setArticles(result.datas);
+    findArticlesByTitle(title.current.value);
   });
 
   return (
     <>
       <h1>記事一覧</h1>
-      <ul>
-        {state.articles.map((article) => {
-          return <li key={article.id}>{article.title}</li>;
-        })}
-      </ul>
+      <ControlGroup>
+        <InputGroup
+          placeholder="タイトルで探す"
+          inputRef={title}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              findArticlesByTitle(title.current.value);
+            }
+          }}
+        />
+        <Button
+          icon="arrow-right"
+          onClick={() => findArticlesByTitle(title.current.value)}
+        />
+      </ControlGroup>
+
+      {state.articles.map((article) => {
+        const openTime = article.openTime;
+        return (
+          <Card key={article.id}>
+            <H5>
+              <a href={ArticleEditPath.makeURI(article.id)}>{article.title}</a>
+            </H5>
+            <div>
+              {openTime.toLocaleDateString()} {openTime.toLocaleTimeString()}
+            </div>
+          </Card>
+        );
+      })}
     </>
   );
 };
